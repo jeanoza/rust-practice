fb2fb63efbea433dfc0db4ce9664bf09094be466
Kyubong <choikb916@gmail.com>
2023-06-16 17:46:07 +0200

    fix(ownership): to continue since 'Variables and Data Interacting with Move'

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -13,6 +13,9 @@ fn main() {
         println!("\n[Variable Scope]");
         //not yet s
         let s = "hello"; // &str is like char * in C/C++(saved in stack)
+        let s2 = s;
+
+        println!("s:{s} s2:{s2}");// save two value in stack 
         println!("test, {}", s.to_owned() + " hi &str"); 
         // s's memory will be free at the last scope
     }
@@ -25,8 +28,15 @@ fn main() {
         let mut s = String::from("hello"); //Dynamic allocation using heaps
         // println!("test, {}", s + " hi String");
         s.push_str(", world!"); // push and re-allocate memory
-        let mut s2 = s + " hi String"; // memory of `s` move to s2
-        println!("{}",  s2);
+        println!("{}",  s);
+
+        let s1 = String::from("hello");
+        let s2 = s1;
+        // s1's value borrowed because of this assignation(moved)
+
+        // println!("{s1}");
+
+
 
     }
 }
