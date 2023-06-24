9cb971ef3e5edd1252df5fe043d662833fc8dcf1
jeanoza <choikb916@gmail.com>
2023-06-24 11:41:44 +0200

    fix:gitignore added

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,21 +1,24 @@
 struct User {
-    active:bool,
+    active: bool,
     username: String,
-    email:String,
-    sign_in_count:u64,
+    email: String,
+    sign_in_count: u64,
 }
 
-fn build_user(email:String, username:String)-> User {
+fn build_user(email: String, username: String) -> User {
     User {
-        active:true,
+        active: true,
         username,
         email,
-        sign_in_count:1,
+        sign_in_count: 1,
     }
 }
 
-fn to_string_user(user: &User) -> String{
-    format!("active:{} username:{} email:{} sign_in_count:{}", &user.active, &user.username, &user.email, &user.sign_in_count)
+fn to_string_user(user: &User) -> String {
+    format!(
+        "active:{} username:{} email:{} sign_in_count:{}",
+        &user.active, &user.username, &user.email, &user.sign_in_count
+    )
 }
 
 fn main() {
@@ -23,16 +26,13 @@ fn main() {
     {
         let user1 = build_user(String::from("jean@gmail.com"), String::from("jean"));
 
-        
         let user2 = User {
-            username:user1.username.clone(),
-            email:String::from("paul@gmail.com"),
+            username: user1.username.clone(),
+            email: String::from("paul@gmail.com"),
             ..user1
         };
         println!("user1:{}", to_string_user(&user1));
 
         println!("user2:{}", to_string_user(&user2));
-
-
     }
 }
