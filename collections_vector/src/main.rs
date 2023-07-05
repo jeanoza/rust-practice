82905d71c57ef38fa001900a1a4137ea015c3376
jeanoza <choikb916@gmail.com>
2023-07-05 19:39:26 +0200

    fix: vector finished

--- collections_vector/src/main.rs
+++ collections_vector/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 fn main() {
     println!("* * * Vector * * *");
     {
@@ -24,9 +26,9 @@ fn main() {
     }
     {
         println!("\n[Read]");
-        let v = vec![1,2,3,4,5];
-        println!("{}",v[2]);
-        println!("{}",v[2]);
+        let v = vec![1, 2, 3, 4, 5];
+        println!("{}", v[2]);
+        println!("{}", v[2]);
 
         let v2 = vec![String::from("hello"), String::from("world")];
         println!("{}", v2[1]);
@@ -41,8 +43,6 @@ fn main() {
         // That's why need to use .clone()
         // let first = v3[0];
         println!("{first}");
-
-
 
         println!("\nmatch using Option<> 1");
         // .get() method is more security as it returns Option<T> which can consider case None using match
@@ -59,17 +59,53 @@ fn main() {
             None => println!("<None>"),
         }
 
-
-        let mut v = vec![1, 2, 3, 4, 5];
+        let v = vec![1, 2, 3, 4, 5];
 
         let first = v.get(0);
-    
+
         // doesn't work because `first` point to `old v` which will be deallocated previus line
         // v.push(6);
         match first {
             Some(_first) => println!("The first element is: {_first}"),
-            None => println!("None")
+            None => println!("None"),
+        }
+    }
+    {
+        println!("\n[Iterating]");
+        let mut v = vec![100, 32, 57];
+        for i in &mut v {
+            *i += 50;
+            //attention:we cannot add or move value in vector in for loop
+            //because, for loop prevents simulatneous modification of whole vector
+            // v.push(42);
+        }
+        for i in &v {
+            println!("{i}");
+        }
+    }
+    {
+        // it will be useful when we know already type
+        // else =>  Chapter 17
+        println!("\n[Enum to store multiple types]");
+        #[derive(Debug)]
+        enum SpreadsheetCell {
+            Int(i32),
+            Float(f64),
+            Text(String),
         }
 
+        let row = vec![
+            SpreadsheetCell::Int(3),
+            SpreadsheetCell::Text(String::from("blue")),
+            SpreadsheetCell::Float(10.12),
+        ];
+
+        println!("{:#?}", row);
+    }
+    {
+        println!("\n[Drop / Delete]");
+        let mut v = vec![1, 4, 5];
+        assert_eq!(v.pop(), Some(5));
+        println!("{:#?}", v);
     }
 }
