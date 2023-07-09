5cfa8476601576bddcf9a0b3013c4d888ac45fb1
jeanoza <choikb916@gmail.com>
2023-07-09 23:16:02 +0200

    not finished

--- collections_exercices/src/main.rs
+++ collections_exercices/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, io};
+
 fn get_median(list: &[i32]) -> f32 {
     let mut v = list.to_vec();
     let len = v.len();
@@ -17,5 +19,50 @@ fn main() {
     {
         println!("[Exercice 1: return median when given a list of integer");
         dbg!(get_median(&[5]));
+    }
+    {
+        println!("[Exercice 3: Employee books");
+        {
+            let mut company: HashMap<String, Vec<String>> = HashMap::new();
+
+            loop {
+                let mut input = String::new();
+
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Failted to readline");
+
+                let cmds: Vec<&str> = input.split_whitespace().collect();
+
+                if cmds.len() < 2 {
+                    println!(
+                        "Invalid input. Please try with:
+    - Add [NAME] to <DEPARTMENT>
+    - List <DEPARTMENT>
+"
+                    );
+                    continue;
+                }
+
+                let action = cmds[0];
+                let name = cmds[1];
+                let department = cmds[3];
+
+                match action {
+                    "Add" => {
+                        company
+                            .entry(String::from(department))
+                            .or_insert(Vec::new())
+                            .push(String::from(name));
+
+                        println!("Added {} to {}", name, department);
+                    }
+                    "List" => {}
+                    _ => {
+                        println!("Input invalid");
+                    }
+                }
+            }
+        }
     }
 }
