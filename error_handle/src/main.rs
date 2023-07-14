224789a9347fcd80c4b0d9bd1bda654ac9ddc3e0
jeanoza <choikb916@gmail.com>
2023-07-14 19:12:25 +0200

    todo:from propagation ~

--- error_handle/src/main.rs
+++ error_handle/src/main.rs
@@ -1,7 +1,9 @@
+use std::{fs::File, io::ErrorKind};
+
 fn main() {
     println!("* * * Error handle * * *");
     {
-        println!("\n[panic!]\n");
+        println!("\n[panic!]");
         {
             println!("before panic");
             // program shut down here
@@ -17,10 +19,45 @@ fn main() {
             println!("after panic");
         }
         {
-            let v = vec![1, 2, 3];
-            //this will call panic because `out of bounds` is vulnerable
-            v[99];
+            let _v = vec![1, 2, 3];
+            // this will call panic because `out of bounds` is vulnerable
+            // v[99];
             //RUST_BACKTRACE=1 cargo run will show backtrace(detail).
+        }
+        println!("\n[Result<T, E>]");
+        {
+            println!("\n- match expression");
+            {
+                let hello_fd = File::open("hello.txt");
+                // cf open with write permission
+                // let hello_fd = OpenOptions::new().write(true).open("hello.txt");
+
+                let _hello_fs = match hello_fd {
+                    Ok(file) => file,
+                    Err(error) => match error.kind() {
+                        ErrorKind::NotFound => match File::create("hello.txt") {
+                            Ok(fc) => fc,
+                            Err(e) => panic!("Problem opening the file {:?}", e),
+                        },
+                        other_error => {
+                            panic!("Problem opening the file {:?}", other_error);
+                        }
+                    },
+                };
+            }
+
+            //.unwrap() try to do given action, call panic and send received Err when ERR
+            println!("\n- unwrap()");
+            {
+                let _hello_fd = File::open("hello.txt").unwrap();
+            }
+
+            // .expect() try to do given action, call panic and send received Err and ERROR_MSG defined by user when ERR
+            // more used by Rustacean rather than unwrap
+            println!("\n- expect([ERROR_MSG])");
+            {
+                let _hello_fd = File::open("hello3.txt").expect("no such files by jeanoza");
+            }
         }
     }
 }
