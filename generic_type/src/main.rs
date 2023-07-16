ffc262fa81c05036972fd2c5aad87552d19a2c3a
jeanoza <choikb916@gmail.com>
2023-07-16 20:18:05 +0200

    fix: generic type initial

--- generic_type/src/main.rs
+++ generic_type/src/main.rs
@@ -1,16 +1,38 @@
+use std::cmp::PartialOrd;
+
 fn largest_number_example() {
-    let number_list = vec![34, 42, 21, 99, 64];
-    let mut largest = &number_list[0];
-    for number in &number_list {
-        if number < largest {
+    let int_list = vec![34, 42, 21, 99, 64];
+    let res = largest_number(&int_list);
+    println!("The largest integer is {}", res);
+
+    let float_list = vec![34, 6000, 21, 99, 64];
+    let res = largest_number(&float_list);
+    println!("The largest float is {}", res);
+
+    let char_list = vec!['c', 'k', 't', 'm'];
+    let res = largest_number(&char_list);
+    println!("The largest(ordered by ascii) char is {}", res);
+}
+
+/**
+ * add `PartialOrd` trait to comparer elements by using its references
+ * :PartialOrd means that we accept only Generic T Type which implement PartialOrd
+ * such as i32, float, char etc
+ */
+fn largest_number<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for number in list {
+        if number > largest {
             largest = number;
         }
     }
-    println!("Largest number is {largest}");
+    largest
 }
 
 fn main() {
     println!("* * * Generic Type * * *");
     println!("[Removing Duplication by Extracting a Function]");
+    println!("[Generic Types] by put <T>");
     largest_number_example();
 }
