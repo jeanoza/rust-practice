dd5049340338f0ff7214c6a74d539ae6c275a5d5
jeanoza <choikb916@gmail.com>
2023-08-02 09:39:03 +0200

    feat(guard): configure guard

--- rust_backend_actix/src/main.rs
+++ rust_backend_actix/src/main.rs
@@ -1,5 +1,7 @@
+use actix_web::guard::GuardContext;
+use actix_web::{get, guard, web, App, HttpResponse, HttpServer, Responder};
+use serde_json::json;
 use std::sync::Mutex;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder, web};
 
 async fn handle_hello_world() -> impl Responder {
     "Hello world!"
@@ -7,7 +9,7 @@ async fn handle_hello_world() -> impl Responder {
 
 #[derive(Debug)]
 struct AppState {
-    app_name:String,
+    app_name: String,
 }
 #[derive(Debug)]
 struct AppStateWithCounter {
@@ -15,42 +17,86 @@ struct AppStateWithCounter {
 }
 
 #[get("/")]
-async fn index(data:web::Data<AppState>) -> impl Responder {
+async fn index(data: web::Data<AppState>) -> impl Responder {
     let app_name = &data.app_name;
-    format!("hello {:?}!",app_name)
+    format!("hello {:?}!", app_name)
 }
 
 #[get("/mutable")]
-async fn mutable_index(data:web::Data<AppStateWithCounter>) -> impl Responder{
+async fn mutable_index(data: web::Data<AppStateWithCounter>) -> impl Responder {
     let mut counter = data.counter.lock().unwrap();
     *counter += 1;
     format!("Request number: {}", counter)
 }
 
 #[get("/mutable2")]
-async fn mutable_index2(data:web::Data<AppStateWithCounter>) -> impl Responder{
+async fn mutable_index2(data: web::Data<AppStateWithCounter>) -> impl Responder {
     format!("counter check(mutable2): {}", data.counter.lock().unwrap())
 }
 
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()>{
+//     let counter = web::Data::new(AppStateWithCounter {
+//         counter: Mutex::new(0),
+//     });
+//     HttpServer::new(move ||{
+//         App::new()
+//             // state which will be used globally at the routes in same scope
+//             .app_data(web::Data::new(AppState {
+//                 app_name:String::from("Actix Web2")
+//             }))
+//             .app_data(counter.clone())
+//             .service(web::scope("/app")
+//                 .route("/index.html", web::get().to(handle_hello_world)))
+//             .service(index)
+//             .service(mutable_index)
+//             .service(mutable_index2)
+//         })
+//         .bind(("localhost", 8080))?
+//         .run()
+//         .await
+// }
+
+//USE guard
+#[get("")]
+async fn get_all_users() -> HttpResponse {
+    HttpResponse::Ok().json(json!({"test": "test"}))
+}
+
+pub fn verify_token(ctx: &GuardContext) -> bool {
+    let auth_header = ctx.head().headers().get("authorization");
+
+    if auth_header.is_none() {
+        false
+    }
+    true
+}
+
+pub fn user_scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/user")
+            .guard(guard::fn_guard(verify_token))
+            .service(get_all_users),
+    );
+}
+
+pub async fn handle_unauthorized() -> HttpResponse {
+    HttpResponse::Unauthorized().json(json!({"error": "Unauthorized"}))
+}
+
+struct ApiGlobalState;
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
-    HttpServer::new(move ||{
+async fn main() -> std::io::Result<()> {
+    let app_data = web::Data::new(ApiGlobalState);
+
+    HttpServer::new(move || {
         App::new()
-            // state which will be used globally at the routes in same scope
-            .app_data(web::Data::new(AppState {
-                app_name:String::from("Actix Web2")
-            }))
-            .app_data(counter.clone())
-            .service(web::scope("/app")
-                .route("/index.html", web::get().to(handle_hello_world)))
-            .service(index)
-            .service(mutable_index)
-            .service(mutable_index2)
-        })
-        .bind(("localhost", 8080))?
-        .run()
-        .await
+            .app_data(app_data.clone())
+            .configure(user_scoped_config)
+            .default_service(web::route().to(handle_unauthorized))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
