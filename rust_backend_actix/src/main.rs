76c6ed0030bad2490e89f677318d3aabacf7882c
jeanoza <choikb916@gmail.com>
2023-08-03 09:17:53 +0200

    host guard

--- rust_backend_actix/src/main.rs
+++ rust_backend_actix/src/main.rs
@@ -57,46 +57,77 @@ async fn mutable_index2(data: web::Data<AppStateWithCounter>) -> impl Responder 
 //         .await
 // }
 
-//USE guard
-#[get("")]
-async fn get_all_users() -> HttpResponse {
-    HttpResponse::Ok().json(json!({"test": "test"}))
-}
-
-pub fn verify_token(ctx: &GuardContext) -> bool {
-    let auth_header = ctx.head().headers().get("authorization");
-
-    if auth_header.is_none() {
-        false
-    }
-    true
-}
-
-pub fn user_scoped_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/user")
-            .guard(guard::fn_guard(verify_token))
-            .service(get_all_users),
-    );
-}
-
-pub async fn handle_unauthorized() -> HttpResponse {
-    HttpResponse::Unauthorized().json(json!({"error": "Unauthorized"}))
-}
-
-struct ApiGlobalState;
-
+//Use Guard ex
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_data = web::Data::new(ApiGlobalState);
-
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
-            .app_data(app_data.clone())
-            .configure(user_scoped_config)
-            .default_service(web::route().to(handle_unauthorized))
+            .service(
+                web::scope("/")
+                    .guard(guard::Host("www.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
+            )
+            .service(
+                web::scope("/")
+                    .guard(guard::Host("users.rust-lang.org"))
+                    .route(
+                        "",
+                        web::to(|| async {
+                            HttpResponse::Ok().body("users"))
+                        }),
+                    ),
+            )
+            .service(web::scope("/").guard(guard::Host("localhost")).route(
+                "",
+                web::to(|| async { HttpResponse::Ok().json(json!({"test": "localhost"})) }),
+            ))
+            .route("/", web::to(HttpResponse::Ok))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
+
+//USE guard
+// #[get("")]
+// async fn get_all_users() -> HttpResponse {
+//     HttpResponse::Ok().json(json!({"test": "test"}))
+// }
+//
+// pub fn verify_token(ctx: &GuardContext) -> bool {
+//     let auth_header = ctx.head().headers().get("authorization");
+//
+//     if auth_header.is_none() {
+//         false
+//     }
+//     true
+// }
+//
+// pub fn user_scoped_config(cfg: &mut web::ServiceConfig) {
+//     cfg.service(
+//         web::scope("/user")
+//             .guard(guard::fn_guard(verify_token))
+//             .service(get_all_users),
+//     );
+// }
+//
+// pub async fn handle_unauthorized() -> HttpResponse {
+//     HttpResponse::Unauthorized().json(json!({"error": "Unauthorized"}))
+// }
+//
+// struct ApiGlobalState;
+//
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     let app_data = web::Data::new(ApiGlobalState);
+//
+//     HttpServer::new(move || {
+//         App::new()
+//             .app_data(app_data.clone())
+//             .configure(user_scoped_config)
+//             .default_service(web::route().to(handle_unauthorized))
+//     })
+//     .bind(("127.0.0.1", 8080))?
+//     .run()
+//     .await
+// }
