c0527fb524c675713fababf137da6e483ba07719
jeanoza <choikb916@gmail.com>
2023-08-06 18:16:11 +0200

    feat(rust_actix_diesel): configuration db is done

---
+++ rust_actix_diesel/src/schema.rs
@@ -0,0 +1,32 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    articles (id) {
+        id -> Int4,
+        created_by -> Int4,
+        #[max_length = 255]
+        title -> Varchar,
+        content -> Text,
+        created_at -> Timestamp,
+        updated_at -> Nullable<Timestamp>,
+        deleted_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+        created_at -> Date,
+        updated_at -> Nullable<Timestamp>,
+        deleted_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::joinable!(articles -> users (created_by));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    articles,
+    users,
+);
